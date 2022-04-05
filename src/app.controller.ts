e6737516123af8f0210aca48a152cc7ef128604e
kyumin0411 <rbalsgood@gmail.com>
2022-04-05 18:51:32 +0900

    [DEV] GET /covid-19 API 구현

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Response } from 'express';
+import * as DTO from 'src/dto/dto';
+import { ApiOperation } from '@nestjs/swagger';
 
-@Controller()
+@Controller('/covid-19')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @ApiOperation({ summary: '코로나 19 감염 현황 데이터 조회' })
+  async getCovidData(
+    @Query() query: DTO.GetCovidDataReqQueryDTO,
+    @Res() res: Response,
+  ) {
+    const result = await this.appService.getCovidData(query);
+    res.status(result.code).json(result);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { lastValueFrom } from 'rxjs';
+import * as DTO from 'src/dto/dto';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private httpService: HttpService) {}
+  async getCovidData(query: DTO.GetCovidDataReqQueryDTO) {
+    try {
+      const urlParams = new URLSearchParams(
+        `serviceKey=${encodeURI(process.env.OPEN_API_KEY)}`,
+      );
+
+      for (const key of Object.keys(query)) {
+        urlParams.set(key, encodeURIComponent(query[key]));
+      }
+
+      const url = process.env.OPEN_API_END_POINT + '?' + urlParams;
+      const openAPIResult = await this.httpService.get(url);
+      const result = await lastValueFrom(openAPIResult);
+
+      if (result?.data && result.data?.response?.header?.resultCode == 0) {
+        return {
+          data: result.data,
+          code: HttpStatus.OK,
+          message: 'Open API 데이터 조회 성공',
+        };
+      }
+
+      return {
+        data: null,
+        code: HttpStatus.CONFLICT,
+        message:
+          result?.data?.response?.header?.resultMsg ??
+          'Open API 데이터 조회 실패',
+      };
+    } catch (error) {
+      throw { code: HttpStatus.CONFLICT, message: error };
+    }
   }
 }
